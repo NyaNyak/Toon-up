03da6aa9736589f5542b66bd058177e2c0397fff
NyaNyak <chsgood8182@naver.com>
2023-03-28 17:44:20 +0900

    add advanced code

---
+++ advanced.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+# 이미지 불러오기
+img = cv2.imread('input.jpg')
+
+# gray scale 변환
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# edge 검출
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+
+# edge 두껍게
+kernel = np.ones((5,5), np.uint8)
+dilated_edges = cv2.dilate(edges, kernel, iterations=1)
+
+# smoothing
+color = cv2.bilateralFilter(img, 9, 80, 20)
+
+# 카툰 스타일 적용
+cartoon = cv2.stylization(color, sigma_s=100, sigma_r=0.3)
+
+# edge 적용
+cartoon = cv2.bitwise_and(cartoon, cartoon, mask=dilated_edges)
+
+# 결과 출력
+cv2.imshow("Cartoon", cartoon)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
